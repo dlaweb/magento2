f6138fa532136c3e3fd8504a8856e6d52561d1e7
macdavid <davidlabbe29@gmail.com>
2018-07-21 13:08:02 +0200

    Controller Actions

---
+++ app/code/Dlaweb/Map/Controller/Map/Index.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: macdavid
+ * Date: 21/07/2018
+ * Time: 12:57
+ */
+
+namespace Dlaweb\Map\Controller\Map;
+
+
+class Index extends \Magento\Framework\App\Action\Action
+{
+	/**
+	 * Index action
+	 *
+	 * @return $this
+	 */
+	public function execute()
+	{
+		// TODO: Implement execute() method.
+		echo "index";
+	}
+}
